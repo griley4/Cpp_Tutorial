554f1b4632b70aaf5602c1327304aee6fdad71cb
griley4 <griley4@vols.utk.edu>
2014-11-28 16:50:17 +0100

    chapter 1 exercises complete

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+
+int main() {{{{
+  
+  std::cout << "Hello Dickweed" << std::endl;
+  /*This ic a multi
+    line comment that starts with \/\*
+    and ends with *\/ duhhhhh*/
+
+}}}}

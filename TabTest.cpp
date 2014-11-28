280b7d193ab294a511c3f21cc00d5a45c48299ac
griley4 <griley4@vols.utk.edu>
2014-11-28 16:19:42 +0100

    started repository and doing examples

---
+++ TabTest.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+int main(){
+  std::cout<<"these are my tab characters"<<std::endl;
+  std::cout<<"\t|"<<std::endl;
+  std::cout<<"\t\t|"<<std::endl;
+  std::cout<<"\t\t\t|"<<std::endl;
+  std::cout<<"\t\t\t\t|"<<std::endl;
+  std::cout<<"\t\t\t\t\t|"<<std::endl;
+  std::cout<<"\t\t\t\t\t\t|"<<std::endl;
+  std::cout<<"\t\t\t\t\t|"<<std::endl;
+  std::cout<<"\t\t\t\t|"<<std::endl;
+  std::cout<<"\t\t\t|"<<std::endl;
+  std::cout<<"\t\t|"<<std::endl;
+  std::cout<<"\t|"<<std::endl;
+  return(0);
+}
